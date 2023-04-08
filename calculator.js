eefb817fb740e58f15a35c12472664b25258d288
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2023-04-07 22:24:56 -0300

    finished

--- calculator.js
+++ calculator.js
@@ -1,20 +1,35 @@
-$(function() {
-  $('button[type="button"]').click(function() {
-      var horas = parseFloat($('#horas').val());
-      var preco = parseFloat($('#preco').val());
-      var servico = $('#servico').val();
-      var empresa = $('#empresa').val();
+document.addEventListener('DOMContentLoaded', function() {
+  var calcularBtn = document.getElementById('calcular');
+  var gerarPdfBtn = document.getElementById('gerar-pdf');
 
-      if (!horas || !preco || !servico || !empresa) {
-        toastr.error('Por favor, preencha todos os campos antes de gerar o PDF.');
-        return;
+  calcularBtn.addEventListener('click', function() {
+    var data = document.getElementById('data').value;
+    var horas = parseFloat(document.getElementById('horas').value);
+    var preco = parseFloat(document.getElementById('preco').value);
+    var servico = document.getElementById('servico').value;
+    var empresa = document.getElementById('empresa').value;
+
+    if (data == '' || horas == '' || preco == '' || servico == '' || empresa == '') {
+      toastr.error('Por favor, preencha todos os campos.');
+    } else {
+      var total = preco * horas;
+      document.getElementById('resultado').innerHTML = 'Valor total do serviço: R$ ' + total.toFixed(2);
     }
+  });
 
-      var total = horas * preco;
+  gerarPdfBtn.addEventListener('click', function() {
+    var data = document.getElementById('data').value;
+    var horas = parseFloat(document.getElementById('horas').value);
+    var preco = parseFloat(document.getElementById('preco').value);
+    var servico = document.getElementById('servico').value;
+    var empresa = document.getElementById('empresa').value;
 
-      $('#resultado').text('O valor total do serviço é R$ ' + total.toFixed(2));
-
-      var doc = new jsPDF();
+    if (data == '' || horas == '' || preco == '' || servico == '' || empresa == '') {
+      toastr.error('Por favor, preencha todos os campos.');
+    } else {
+      var total = preco * horas;
+      var doc = new jsPDF();            
+      
       doc.setFontSize(20);
       doc.setFont('helvetica', 'bold');
       doc.setTextColor('#333');
@@ -23,16 +38,19 @@ $(function() {
       doc.line(20, 30, 190, 30);
       doc.setFontSize(14);
       doc.setFont('helvetica', 'normal');
-      doc.text('Número de horas trabalhadas:', 20, 50);
-      doc.text(horas.toString(), 140, 50);
-      doc.text('Preço da hora:', 20, 60);
-      doc.text('R$ ' + preco.toFixed(2), 140, 60);
-      doc.text('Serviço realizado:', 20, 70);
-      doc.text(servico, 140, 70);
-      doc.text('Empresa:', 20, 80);
-      doc.text(empresa, 140, 80);
-      doc.text('Valor total do serviço:', 20, 90);
-      doc.text('R$ ' + total.toFixed(2), 140, 90);
+      doc.text('Data:', 20, 50);
+      doc.text(data, 140, 50);
+      doc.text('Número de horas trabalhadas:', 20, 60);
+      doc.text(horas.toString(), 140, 60);
+      doc.text('Preço da hora:', 20, 70);
+      doc.text('R$ ' + preco.toFixed(2), 140, 70);
+      doc.text('Serviço realizado:', 20, 80);
+      doc.text(servico, 140, 80);
+      doc.text('Empresa:', 20, 90);
+      doc.text(empresa, 140, 90);
+      doc.text('Valor total do serviço:', 20, 100);
+      doc.text('R$ ' + total.toFixed(2), 140, 100);
       doc.save('detalhes_servico.pdf');
+    }
   });
 });
